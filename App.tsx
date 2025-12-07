9de85b710e07d62feef82639e256e785bd06c75a
re000start-alt <re000start@gmail.com>
2025-12-07 13:22:20 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,11 +4,27 @@ import AppNavigator from "./src/navigation";
 import { AuthProvider } from "./src/context/AuthContext";
 import * as Notifications from "expo-notifications";
 import * as Device from "expo-device";
+import { initializeApp } from "firebase/app";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from "@env";
 
+// Notification handler
 Notifications.setNotificationHandler({
-  handleNotification: async () => ({ shouldShowAlert: true, shouldPlaySound: true, shouldSetBadge: false }),
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
 });
 
+// Register for local notifications
 async function registerForLocalNotifications() {
   try {
     if (!Device.isDevice) {
@@ -27,8 +43,22 @@ async function registerForLocalNotifications() {
   }
 }
 
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
 export default function App() {
   useEffect(() => {
+    // Initialize Firebase
+    initializeApp(firebaseConfig);
+
+    // Register notifications
     registerForLocalNotifications();
   }, []);
 
